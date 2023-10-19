3c0962fa8b1d76b5032b466d92f89182b50c1c08
mechztcc <dojathan@gmail.com>
2023-10-19 09:32:05 -0300

    feat: implementing authorization with http interceptor

--- src/modules/photo/controllers/photo.controller.ts
+++ src/modules/photo/controllers/photo.controller.ts
@@ -1,6 +1,7 @@
 import {
   Controller,
   Get,
+  Headers,
   Param,
   Post,
   Res,
@@ -9,9 +10,11 @@ import {
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
+import { HttpInterceptor } from 'src/shared/interceptors/http/http.interceptor';
 import { PhotoService } from '../services/photo.service';
 
 @Controller('photo')
+@UseInterceptors(HttpInterceptor)
 export class PhotoController {
   constructor(private readonly photoService: PhotoService) {}
 
@@ -32,8 +35,10 @@ export class PhotoController {
       },
     }),
   )
-  create(@UploadedFile() file: Express.Multer.File) {
-    return this.photoService.create({ name: file.filename, userId: 1 });
+  create(@Headers() headers, @UploadedFile() file: Express.Multer.File) {
+    const user = headers.user;
+
+    return this.photoService.create({ name: file.filename, userId: user.id });
   }
 
   @Get('list/:userId')
---
+++ src/shared/interceptors/http/http.interceptor.ts
@@ -0,0 +1,31 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class HttpInterceptor implements NestInterceptor {
+  constructor(private jwtService: JwtService) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+
+    const token = request.headers['authorization'];
+
+    const decoded = this.jwtService.decode(token);
+
+    if (!token || !decoded) {
+      throw new UnauthorizedException('Token has not provided');
+    }
+
+    request.headers['user'] = decoded;
+    console.log(decoded);
+
+    return next.handle();
+  }
+}
